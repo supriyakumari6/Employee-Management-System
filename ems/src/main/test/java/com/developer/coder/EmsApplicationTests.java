44792b99d9d9d5848effcb71c4030e2690aa21e3
supriyakumari6 <supriyakumari98011@gmail.com>
2026-05-22 19:19:36 +0530

    Create EmsApplicationTests.java

---
+++ ems/src/main/test/java/com/developer/coder/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.coder.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
