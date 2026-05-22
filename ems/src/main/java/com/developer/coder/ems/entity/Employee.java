03d89b82e485c4a05deef8e7f80c3e86587b5576
supriyakumari6 <supriyakumari98011@gmail.com>
2026-05-22 19:34:32 +0530

    Create Employee.java

---
+++ ems/src/main/java/com/developer/coder/ems/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.developer.coder.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name ="first_name")
+    private String firstName;
+
+    @Column(name ="last_name")
+    private String lastName;
+
+    @Column(name ="email", nullable = false, unique = true)
+    private String email;
+}
