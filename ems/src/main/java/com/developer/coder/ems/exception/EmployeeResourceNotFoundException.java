92131bbb890b9e125911b0a7be050088ac1b85db
supriyakumari6 <supriyakumari98011@gmail.com>
2026-05-22 19:32:11 +0530

    Create EmployeeResourceNotFoundException.java

---
+++ ems/src/main/java/com/developer/coder/ems/exception/EmployeeResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.developer.coder.ems.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeResourceNotFoundException extends  RuntimeException {
+
+    public  EmployeeResourceNotFoundException(String message){
+        super(message);
+    }
+}
